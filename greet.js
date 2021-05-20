f0ae9e748d963cb951ef4d256eed4f8255ccee5c
Vhonani24 <vhonani.ramu@gmail.com>
2021-05-20 16:43:18 +0200

    updated

---
+++ greet.js
@@ -0,0 +1,11 @@
+//get reference to the name input elements
+const theNameElement = document.querySelector("#container");
+//get reference to the text area element
+const greetMessage = document.querySelector("#greetMessage");
+//get reference to the greet button
+const greetButton = document.querySelector("#success-outlined");
+//get reference to the greet button
+const clearButton = document.querySelector("#danger-outlined");
+//get reference to the counter
+const counter = document.querySelector("#counter");
+
