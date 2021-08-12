6d2ecdf7e778e70f1c0ca85e9d3cc7035260e455
Vhonani24 <vhonani.ramu@gmail.com>
2021-08-12 16:45:34 +0200

    updated

--- index.js
+++ index.js
@@ -1,45 +1,46 @@
 const express = require('express');
 const app = express();
 const exphbs = require('express-handlebars');
-
-var session = require('express-session');
-var cookieParser = require('cookie-parser');
-var flash = require('express-flash');
-var sessionStore = new session.MemoryStore;
-
-
 const Greetings = require('./greetings');
-
-
 const greetings = Greetings();
 app.engine('handlebars', exphbs({ layoutsDir: 'views/layouts/' }));
 app.set('view engine', 'handlebars');
 
-app.use(cookieParser('secret'));
-app.use(session({
-    cookie: { maxAge: 60000 },
-    store: sessionStore,
-    saveUninitialized: true,
-    resave: 'true',
-    secret: 'secret'
-}));
-app.use(flash());
-
-
-
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json())
+//just testing flash messages
+const cookieParser = require('cookie-parser')
+const session = require('express-session')
+app.use(cookieParser('secret'))
 
-var greets = "";
+app.use(session({cookie: {maxAge: null},
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true,
+    secret: 'secret'
+   
+}))
+
+//app.use(flash());
+//flash message middleware
+app.use((req, res, next)=>{
+    res.locals.message = req.session.message
+    delete req.session.message
+    next()
+})
+  
 
 
+
+//-------------------------------
 app.get('/', function (req, res) {
     res.render('index', {
         name: greetings.getNames(),
         greetMessage: greetings.greet(),
         counter: greetings.setCounter(),
-        expressFlash: req.flash('success'), sessionFlash: res.locals.sessionFlash
+       
+        
         
 
     });
@@ -47,15 +48,34 @@ app.get('/', function (req, res) {
 });
 
 
-app.post('/greeted', function (req, res) {
-    greetings.setNames(req.body.name);
-    greetings.setLang(req.body.lang);
-    greetings.pushNames(req.body.name);
-    greetings.setCounter();
-    req.flash('info', 'Flash Message Added');
-    res.redirect("/");
+app.post('/', function (req, res) {
+    if(req.body.name == "" || req.body.lang == ""){
+        req.session.message = {
+            type: 'danger',
+            intro: 'Empty fields!',
+            message: 'please enter the requested information'
+
+        }
+
+    }
+    else{
+        greetings.setNames(req.body.name);
+        greetings.setLang(req.body.lang);
+        greetings.pushNames(req.body.name);
+        greetings.setCounter();
+        res.redirect("/");
+        
+
+    }
+
+
+
+    
+
 
 });
+
+
 
 const PORT = process.env.PORT || 2021;
 app.listen(PORT, function () {
